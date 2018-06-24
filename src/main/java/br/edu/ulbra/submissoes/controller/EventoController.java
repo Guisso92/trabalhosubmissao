4bcb459cd6fa34a7e83ac5168a761c06fbea7275
Guilherme Guisso <twguisso@gmail.com>
2018-06-24 16:38:06 -0300

    Criação de controllers usuario e eventos

--- src/main/java/br/edu/ulbra/submissoes/controller/EventoController.java
+++ src/main/java/br/edu/ulbra/submissoes/controller/EventoController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/usuarios")
-public class UserController {
-	
+@RequestMapping("/eventos")
+public class EventoController {
+
 	@GetMapping("/cadastrar")
 	public String novo() {
-		return "/usuarios/novo";
+		return "/eventos/novo";
 	}
-
+	
 }
