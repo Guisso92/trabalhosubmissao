8d830941b6ee3c667fd73c763eaf14bd08612d7b
Guilherme Guisso <twguisso@gmail.com>
2018-06-27 18:13:44 -0300

    Criação de controllers usuario e eventos 2

--- src/main/java/br/edu/ulbra/submissoes/controller/IndexController.java
+++ src/main/java/br/edu/ulbra/submissoes/controller/IndexController.java
@@ -2,6 +2,7 @@ package br.edu.ulbra.submissoes.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -9,6 +10,11 @@ public class IndexController {
 	@GetMapping("/")
 	public String index() {
 		return "/index.html";
+	}
+	
+	@GetMapping("/login")
+	public ModelAndView loginForm(){
+		return new ModelAndView("login/login");
 	}
 
 }
---
+++ src/main/java/br/edu/ulbra/submissoes/model/Evento.java
@@ -0,0 +1,59 @@
+package br.edu.ulbra.submissoes.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Evento {
+
+	@Column
+	private Long id;
+	@Column
+	private String nome;
+	@Column
+	private String dataCadastro;
+	@Column
+	private String dataAbertura;
+	@Column
+	private String dataFechamento;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(String dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public String getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(String dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public String getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(String dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+}
